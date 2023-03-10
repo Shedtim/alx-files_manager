56497eada265a543053cbb501ea5b0d14cec218b
Victoria-20 <victoriaocon111@gmail.com>
2023-03-10 11:34:21 +0300

    0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+import { createClient } from 'redis';
+
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = createClient({
+      host: 'localhost',
+      port: 6379,
+    }).on('error', (err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  async get(key) {
+    // convert the get method to return a promise
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return setAsync(key, value, duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
