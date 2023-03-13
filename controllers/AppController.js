e077974a71c2da3d5fb9d3892cd468564dba7034
Victoria-20 <victoriaocon111@gmail.com>
2023-03-13 11:03:27 +0300

    2. First API

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+const Redis = require('../utils/redis');
+const Database = require('../utils/db');
+
+async function getStatus(req, res) {
+  const redisStatus = await Redis.isAlive();
+  const dbStatus = await Database.isAlive();
+  res.status(200).json({ redis: redisStatus, db: dbStatus });
+}
+
+async function getStats(req, res) {
+  const userCount = await Database.nbUsers();
+  const filesCount = await Database.nbFiles();
+  res.status(200).json({ users: userCount, files: filesCount });
+}
+
+module.exports = [getStats, getStatus];
